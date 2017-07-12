d7b51ff92413981b28cfea305d258acacadfd6a1
trixtar <magisterludi1789@gmail.com>
2017-07-12 20:31:11 -0300

    First sync

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+// var http = require('http');
+// var https = require('https');
+
+app.use('/lib', express.static(__dirname + '/node_modules'));
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+  console.log(req.query);
+});
