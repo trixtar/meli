c4f564227b37655666390949f7465b8654d43909
trixtar <magisterludi1789@gmail.com>
2017-07-14 01:40:05 -0300

    api1

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
-// var http = require('http');
-// var https = require('https');
+var https = require('https');
 
 app.use('/lib', express.static(__dirname + '/node_modules'));
 app.use('/public', express.static(__dirname + '/public'));
@@ -13,6 +12,34 @@ app.get('/', function (req, res) {
 
 app.get('/items', function (req, res) {
   res.sendFile(__dirname + '/public/views/busqueda.html');
+});
+
+app.get('/api/items', function(req, res) { 
+	https.get('https://api.mercadolibre.com/sites/MLA/search' + req.params.QUEVAACA, function(response) {
+		var body='';
+		response.on('data', function(d) {
+			body += d;
+		});
+		response.on('end', function() {
+			body = JSON.parse(body); //convierte la data string en json y la consolea en el back
+			/* 
+			for (var i=0; i < body.data.length; i++) {
+				/* var persona = body.data[i];
+				persona.full_name = persona.first_name + ' ' + persona.last_name;
+				body.data[i] = persona; 
+
+				var persona = body.data[i];
+				var personaNueva = {};
+				personaNueva.avatar = persona.avatar;
+				personaNueva.id = persona.id;
+				personaNueva.full_name = persona.first_name + ' ' + persona.last_name;
+				body.data[i] = personaNueva;
+
+			}
+			*/
+			res.send(body);
+		}); 
+	});
 });
 
 app.listen(3000, function () {
