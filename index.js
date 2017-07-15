813dd98d496a433e0f7ab121e2731dea6f5bfb46
trixtar <magisterludi1789@gmail.com>
2017-07-14 21:06:30 -0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
+var request = require('request');
+//var https = require('https');
 var app = express();
-var https = require('https');
 
 app.use('/lib', express.static(__dirname + '/node_modules'));
 app.use('/public', express.static(__dirname + '/public'));
@@ -14,6 +15,14 @@ app.get('/items', function (req, res) {
   res.sendFile(__dirname + '/public/views/busqueda.html');
 });
 
+app.get('/api/items', function (req, res) {
+  request('https://api.mercadolibre.com/sites/MLA/search?q=' + req.query.q, function (error, response, body) {
+    res.send(body);
+  });
+});
+
+
+/*
 app.get('/api/items', function(req, res) { 
 	https.get('https://api.mercadolibre.com/sites/MLA/search' + req.params.QUEVAACA, function(response) {
 		var body='';
@@ -22,7 +31,7 @@ app.get('/api/items', function(req, res) {
 		});
 		response.on('end', function() {
 			body = JSON.parse(body); //convierte la data string en json y la consolea en el back
-			/* 
+			 
 			for (var i=0; i < body.data.length; i++) {
 				/* var persona = body.data[i];
 				persona.full_name = persona.first_name + ' ' + persona.last_name;
@@ -36,11 +45,12 @@ app.get('/api/items', function(req, res) {
 				body.data[i] = personaNueva;
 
 			}
-			*/
+			
 			res.send(body);
 		}); 
 	});
 });
+*/
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
